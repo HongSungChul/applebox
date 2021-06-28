247759c1676eca87c609f1c8c053f2e4786b4df0
HongSungChul <mbagix@gmail.com>
2021-06-28 12:35:21 +0900

    Create vpn_client.cmd

---
+++ vpn_client.cmd
@@ -0,0 +1,36 @@
+# Table of Contents
+
+1. [현 시스템 OS 관련하여](#os)
+2. [API 참조](#api)
+3. [Control Board](#control-board)
+4. [django](#django)
+5. [raspberrypi](#raspberrypi)
+6. [setup](#setup)
+7. [터치스크린로그](/logsKivy)
+8. [데몬로그](/logsDjango)
+9. [푸시로그](/logsPush)
+10. [new install](/newinstall)
+11. [시험절차서](/test)
+
+
+# django
+
+[django](/django) 참조
+
+
+# raspberrypi
+
+[raspberrypi](/raspberrypi) 참조
+
+# setup
+
+[setup](/setup) 참조
+
+
+# newinstall
+
+[newinstall](/newinstall) 참조
+
+# 시험절자처
+
+[test](/test) 참조
